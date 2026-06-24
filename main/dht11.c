ecea0b9dc42fcf09635de1d2a79ed5eacb9b898f
Saurabh <71336366+Speedyboy001@users.noreply.github.com>
2026-06-24 14:59:19 +0530

    Start adding DHT11 init

--- main/dht11.c
+++ main/dht11.c
@@ -20,9 +20,9 @@ void dht_start_signal(void)
    ESP_ERROR_CHECK(gpio_config(&io_config));
 
    ESP_ERROR_CHECK(gpio_set_level(DHT_PIN,PIN_LOW));
-   vTaskDelay(pdMS_TO_TICKS(18));
+   vTaskDelay(pdMS_TO_TICKS(DHT_INIT_TIME_US));
    ESP_ERROR_CHECK(gpio_set_level(DHT_PIN,PIN_HIGH));
-   esp_rom_delay_us(40);
+   esp_rom_delay_us(DHT_WAIT_TIME_US);
 
 //    io_config.mode = GPIO_MODE_INPUT;
 //    io_config.pin_bit_mask = (1ULL << DHT_PIN);
@@ -31,18 +31,46 @@ void dht_start_signal(void)
 }
 
 
-void read_data_raw(void)
+
+uint8_t read_raw_byte(void)
+{
+    uint8_t packet = 0;
+    for (size_t i = 0; i < 8; i++)
+    {
+        while (gpio_get_level(DHT_PIN) == PIN_LOW);
+        esp_rom_delay_us(30);
+        if(gpio_get_level(DHT_PIN) == PIN_HIGH)
+        {
+            packet |= (1 << (7-i));
+        }
+        while (gpio_get_level(DHT_PIN) == PIN_HIGH);    
+    } 
+    return packet; 
+}
+
+void read_data_raw(uint8_t *data, uint8_t len)
 {
     dht_start_signal();
     uint32_t start_time = xTaskGetTickCount();
     while (gpio_get_level(DHT_PIN) == PIN_HIGH)
     {
-        if((xTaskGetTickCount() - start_time ) > DHT_DATA_TIMEOUT)
+        if((xTaskGetTickCount() - start_time ) > DHT_DATA_TIMEOUT_US)
         {
             ESP_LOGI(DHT,"Time out err");
             return;
         }
     }
-/*work continus*/
-    
+    if(gpio_get_level(DHT_PIN) == PIN_LOW)
+    {
+        esp_rom_delay_us(DHT_TOTAL_WAIT_LOW_US);
+        if(gpio_get_level(DHT_PIN) == PIN_HIGH)
+        {
+            esp_rom_delay_us(DHT_TOTAL_WAIT_HIGH_US);
+            for(int idx = 0; idx < len; idx++)
+            {
+                data[idx]= read_raw_byte();
+            }
+        }
+    }
+/*work continus*/   
 }
--- main/dht11.h
+++ main/dht11.h
@@ -1,14 +1,17 @@
 
 
-static const char* ERR_TIMEOUT = "ERR_DHT_TIMEOUT";
-static const char* ERR_CRC = "ERR_DHT_CRC";
+static const char* ERR_TIMEOUT = 231;      //"ERR_DHT_TIMEOUT";
+static const char* ERR_CRC = 232;           //"ERR_DHT_CRC";
 
 
 #define DHT_PIN                 GPIO_NUM_5
 
 
-#define DHT_INIT_TIME           18
-#define DHT_WAIT_TIME           40
-#define DHT_TOTAL_DATA_TIME     80
-#define DHT_DATA_TIMEOUT        500
+#define DHT_INIT_TIME_US            18
+#define DHT_WAIT_TIME_US            40
+#define DHT_TOTAL_WAIT_LOW_US       80
+#define DHT_TOTAL_WAIT_HIGH_US      80
+#define DHT_DATA_TIMEOUT_US         500
 
+
+#define DHT_SAMPLE_TIME_US      30
